a8647276293d691edb69ae2da3711b822f67477e
Dax <157641@student.horizoncollege.nl>
2021-09-01 13:29:43 +0200

    Uitwerking van de exercise

---
+++ Player.php
@@ -0,0 +1,28 @@
+<?php
+
+class Player
+{
+    private $name = "john doe";
+    private $pinsThrown = [];
+
+    public function __construct($name)
+    {
+        $name;
+    }
+
+    public function throwPins($one, $two)
+    {
+        $score = [$one, $two];
+    }
+
+    public function getPinsThrown()
+    {
+        $score = $score;
+        echo $score;
+    }
+
+    public function getName()
+    {
+        echo $name;
+    }
+}
---
+++ testPlayer.php
@@ -0,0 +1,10 @@
+<?php
+
+require('Player.php');
+$player = new Player('John Doe');
+$player->throwPins(rand(1, 5), rand(1, 5));
+$score = $player->getPinsThrown();
+echo $player->getName() . " heeft de eerste keer " . $score[0][0] . " gegooid" . PHP_EOL;
+echo $player->getName() . " heeft de eerste keer " . $score[0][1] . " gegooid";
+
+?>
