12f2e7ca1cf92156fdfedc33b86f797bf054c0cb
Dax <157641@student.horizoncollege.nl>
2021-09-02 14:28:15 +0200

    Uitwerking van de exercise

---
+++ ScoreBoard.php
@@ -0,0 +1,24 @@
+<?php
+
+class ScoreBoard
+{
+    private $scores = [];
+    private $players = [player];
+
+    public function __construct($players)
+    {
+    }
+    private function calculatePlayerscore($player) 
+    {
+    }
+    private function calculateAllscores()
+    {
+    }
+    public function displayScores()
+    {
+    }
+    public function displayWinner()
+    {
+    }
+}
+?>
