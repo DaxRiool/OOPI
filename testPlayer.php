2662ceea381d12656cca8065675e9ef38d911ff8
Dax <157641@student.horizoncollege.nl>
2021-09-02 13:27:59 +0200

    Uitwerking van de exercise

--- testPlayer.php
+++ testPlayer.php
@@ -5,8 +5,6 @@ $player = new Player('John Doe');
 $player->throwPins(rand(1, 5), rand(1, 5));
 $score = $player->getPinsThrown();
 echo $player->getName() . " heeft de eerste keer " . $score[0] . " gegooid" . PHP_EOL;
-$player->throwPins(rand(1, 5), rand(1, 5));
-$score = $player->getPinsThrown();
 echo $player->getName() . " heeft de tweede keer " . $score[1] . " gegooid";
 
 ?>
